a9d1339bbc50c1383501a6efd53c873d57c6ae29
deboravpereira <100171504+deboravpereira@users.noreply.github.com>
2023-12-18 10:37:17 -0300

    Update new_scraper.py

--- new_scraper.py
+++ new_scraper.py
@@ -9,7 +9,7 @@ import pandas as pd
 START_URL = "https://exoplanets.nasa.gov/exoplanet-catalog/"
 
 # Webdriver
-browser = webdriver.Chrome("D:/Setup/chromedriver_win32/chromedriver.exe")
+browser = webdriver.Chrome("./chromedriver.exe")
 browser.get(START_URL)
 
 time.sleep(10)
